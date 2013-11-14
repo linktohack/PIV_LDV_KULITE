6c495012c0600e92ba06ea33b14f87aae5ac4de9
Quang Linh LE <linktohack@github.com>
2013-11-14 12:57:39 +0100

    HEADER Add header to load from notebook

---
+++ header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+%matplotlib inline
+
+import numpy as np
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import plot, show, xlim, ylim, grid, legend
+
+matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
