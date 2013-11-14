dff57762732c66d5c8f14d06a03764699ba2e638
Quang-Linh LE <linktohack@gmail.com>
2013-11-14 23:19:12 +0100

    HEADER Add title and label

--- header.py
+++ header.py
@@ -5,6 +5,6 @@ import numpy as np
 
 import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import plot, show, xlim, ylim, grid, legend
+from matplotlib.pyplot import plot, show, title, xlabel, ylabel, xlim, ylim, grid, legend
 
 matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
