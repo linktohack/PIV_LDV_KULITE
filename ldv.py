7f0cb85d78043897b4510bc95ce6c960241f8e5b
Quang Linh LE <linktohack@github.com>
2013-11-14 13:16:12 +0100

    LDV datarate return both dr and position

--- ldv.py
+++ ldv.py
@@ -4,9 +4,11 @@ import numpy as np
 
 def datarate(fn, rn=None, debug=False):
     """Calculate datarate of a file fn or a set of file fn with range rn"""
-    def dr1(fn):
+    def _dr(fn):
+        """Calculate datarate of one file"""
         with open(fn, 'r') as f:
-            [f.next() for _ in xrange(6)]    
+            header = [f.next() for _ in xrange(6)]
+            pos = np.array(header[3].strip()[:-4].split(' mm;')).astype(np.float64)
             firstline =  f.next()
         
             for lastline in f:
@@ -16,22 +18,26 @@ def datarate(fn, rn=None, debug=False):
             last = np.array(lastline.strip().split('\t')).astype(np.float64)
 
             dr = (last[0] - first[0])/(last[1] - first[1])*1000   
-            return dr
+            return dr, pos
     
     if rn is None:
         if debug:
             print fn
-        return dr1(fn)
+        return _dr(fn)
     else:
         dr = []
+        pos = []
         for i in xrange(rn[0], rn[1]):
-            fn1 = fn % i
-            if os.path.exists(fn1):
+            fn_ = fn % i
+            if os.path.exists(fn_):
                 if debug:
                     print fn1
-                dr.append(dr1(fn1))
+                dr_, pos_ = _dr(fn_)
+                dr.append(dr_)
+                pos.append(pos_)
             else:
                 if debug:
                     print '-', fn1
                 
-        return dr
+        return dr, pos
+
