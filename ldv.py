acef07e95285efcd1cfc5f67be5b96584c46f2a5
Quang Linh LE <linktohack@gmail.com>
2014-03-07 16:56:18 +0100

    [Tidy] LDV Remove some unwanted spaces

--- ldv.py
+++ ldv.py
@@ -14,16 +14,16 @@ def datarate(fn, debug=False):
             header = [f.next() for _ in xrange(6)]
             pos = np.array(header[3].strip()[:-4].split(' mm;')).astype(np.float64)
             firstline =  f.next()
-        
+
             for lastline in f:
                 pass
 
-            first = np.array(firstline.strip().split('\t')).astype(np.float64)        
+            first = np.array(firstline.strip().split('\t')).astype(np.float64)
             last = np.array(lastline.strip().split('\t')).astype(np.float64)
 
-            dr = (last[0] - first[0])/(last[1] - first[1])*1000   
-            return pos, dr 
-    
+            dr = (last[0] - first[0])/(last[1] - first[1])*1000
+            return pos, dr
+
     fl = sorted(glob.glob(fn))
     if not fl:
         raise IOError('File not found! %s' % fn)
@@ -41,7 +41,7 @@ def datarate(fn, debug=False):
             pos_, dr_ = _dr(fn_)
             pos.append(pos_)
             dr.append(dr_)
-                
+
         return pos, dr
 
 
@@ -55,7 +55,7 @@ def quantities(fi, fo=None, rot=0, off=(), debug=False):
             pos = np.array(header[3].strip()[:-4].split(' mm;')).astype(np.float64)
             if debug:
                 print '[x y z] = ', pos
-            
+
             lda = np.loadtxt(fn, skiprows=6, usecols=(3,4), dtype=np.float64)
             if debug:
                 print 'Number of samples = ', lda.shape[0]
@@ -64,11 +64,11 @@ def quantities(fi, fo=None, rot=0, off=(), debug=False):
             a2 = np.sin((1./4 - rot/180.)*np.pi)
             b1 = np.cos((-1./4 - rot/180.)*np.pi)
             b2 = np.sin((-1./4 - rot/180.)*np.pi)
-            
+
             u = lda[:,0]*a1 + lda[:,1]*b1
             v = lda[:,0]*a2 + lda[:,1]*b2
             U = np.mean((u**2 + v**2)**0.5)
-            
+
             u = u - u.mean() # ubar, vbar
             v = v - v.mean()
 
@@ -76,7 +76,7 @@ def quantities(fi, fo=None, rot=0, off=(), debug=False):
             uvbar = np.abs((u*v).mean())**0.5
 
             return pos, U, k, uvbar
-        
+
     fl = sorted(glob.glob(fi))
     if not fl:
         raise IOError(2, 'No such file or directory' % fi)
@@ -98,7 +98,7 @@ def quantities(fi, fo=None, rot=0, off=(), debug=False):
             U.append(U_)
             k.append(k_)
             uvbar.append(uvbar_)
-        
+
     if fo is not None:
         po = fo[:fo.rfind('/')]
         if not os.path.exists(po):
