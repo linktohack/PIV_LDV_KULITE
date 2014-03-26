dbd64c4af436ca0c3629142604e3f1495364b523
Quang Linh LE <linktohack@gmail.com>
2014-03-26 16:30:12 +0100

    UTILS move ldv_wavelet_rec in LDV to wavelet_rec in UTILS

--- utils.py
+++ utils.py
@@ -42,4 +42,55 @@ def norm_s(sig, Pxx):
             Pxx[:,i] = norm_s_1d(sig[:,i], Pxx[i])
         return Pxx
 
+def wavelet_rec(t, sig, fs=1e5, NFFT=4096, stw=0.3, err=1e-3):
+    """Reconstruct signal by Wavelet Technique
+    Module used by JAUNET"""
+    nsig1 = sig.shape[1]
+    n1rec = NFFT
+    nmin = 2
+    ferec = np.float64(fs)
+    erconv = np.float64(err)
+
+    no_part = int(t[-1]/(n1rec-1)*ferec)
+
+    s1rec_all = np.empty([n1rec*no_part,nsig1], dtype=np.float64, order='F')
+    at1rec_all = np.arange(n1rec*no_part)/ferec
+
+    part = 0
+    n0 = 0
+    n1 = 0
+    for i in xrange(len(t)):
+        if t[i] > at1rec_all[n1rec*(part+1)-1]:
+            n0 = n1
+            n1 = i + 1
+
+            at1rec = at1rec_all[n1rec*part:n1rec*(part+1)]
+            at1rec = at1rec - at1rec_all[n1rec*part]
+            s1rec = np.empty([n1rec,nsig1], dtype=np.float64, order='F')
+
+            at1 = t[n0:n1] - at1rec_all[n1rec*part]
+            s1 = sig[n0:n1,:]
+            s1[:,0] = s1[:,0] - np.mean(s1[:,0])
+
+            dt1 = at1[1:] - at1[:-1]
+            mask = np.where(dt1 > 1/ferec)[0]
+            nfil = len(mask)
+
+            at2 = at1[mask]
+            s2 = s1[mask,:]
+
+
+            waveletrec.waveletrecldv(at2, s2, at1rec, s1rec, erconv, stw, nmin,
+                    nn=nfil, nsig=nsig1, nn2=n1rec)
+
+
+            s1rec_all[n1rec*part:n1rec*(part+1),:] = s1rec
+
+            # Next part
+            part += 1
+            if n1rec*(part+1) > len(s1rec_all):
+                break
+
+    return at1rec_all, s1rec_all
+
 # vim:set sw=4 ts=4 tw=78:
