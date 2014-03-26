af05aad10271262cb265a39dbff5e5f45a50e1f1
Quang Linh LE <linktohack@gmail.com>
2014-03-26 16:21:47 +0100

    UTILS add argpeaks function to find the peaks

--- utils.py
+++ utils.py
@@ -7,4 +7,22 @@ def correlation(a, v):
 
     return np.correlate(a, v, 'same')
 
+def argpeaks(s, deviation=3, lookahead=10, cb=None):
+    """Find the peaks of the signal within `deviation`*sigma range,
+    exclude the fake signal within `lookahead` range.
+
+    Simple version.
+    """
+    ds = s[1:] - s[:-1]
+    st = np.where(ds > deviation*s.std())[0]
+    if cb:
+        cb('First try, find the peaks', st)
+
+    for i in xrange(st.shape[0]-1, 0, -1):
+        if (st[i] - st[i-1]) < 10:
+            if cb:
+                cb('Fake peak in st: ', [st[i], st[i-1]])
+            st = np.delete(st, i)
+    return st
+
 # vim:set sw=4 ts=4 tw=78:
