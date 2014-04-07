cce2a64c425be26a38384a9c9b6a3354dcc83908
Quang Linh LE <linktohack@gmail.com>
2014-04-07 14:11:40 +0200

    HEADER Add subplot and figure to header

--- header.py
+++ header.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import plot, show, title, xlabel, ylabel, xlim, ylim, grid, legend
+from matplotlib.pyplot import plot, subplot, figure, show, title, xlabel, ylabel, xlim, ylim, grid, legend
 
 matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
 matplotlib.rcParams['axes.grid'] = True
